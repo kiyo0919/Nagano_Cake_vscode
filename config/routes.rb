f06cce63d7a8eb7dc312bd09c921da3c956306a9
kiyo0919 <kakinoki0919@gmail.com>
2021-10-18 11:05:51 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,12 @@ Rails.application.routes.draw do
 
   devise_for :end_users, skip: :all
   devise_scope :end_user do
-    get 'end_users/sign_in' => 'end_users/sessions#new', as: 'new_end_user_session'
-    post 'end_users/sign_in' => 'end_users/sessions#create', as: 'end_user_session'
-    delete 'end_users/sign_out' => 'end_users/sessions#destroy', as: 'destroy_end_user_session'
-    get 'end_users/sign_up' => 'end_users/registrations#new', as: 'new_end_user_registration'
-    post 'end_users' => 'end_users/registrations#create', as: 'end_user_registration'
-    get 'end_users/password/new' => 'end_users/passwords#new', as: 'new_end_user_password'
+    get 'customers/sign_in' => 'end_users/sessions#new', as: 'new_end_user_session'
+    post 'customers/sign_in' => 'end_users/sessions#create', as: 'end_user_session'
+    delete 'customers/sign_out' => 'end_users/sessions#destroy', as: 'destroy_end_user_session'
+    get 'customers/sign_up' => 'end_users/registrations#new', as: 'new_end_user_registration'
+    post 'customers' => 'end_users/registrations#create', as: 'end_user_registration'
+    get 'customers/password/new' => 'end_users/passwords#new', as: 'new_end_user_password'
   end
     
   namespace :admin do
